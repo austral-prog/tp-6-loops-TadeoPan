caacb003069f08d543102506164ed40005b3cfde
TadeoPan <taapan@mail.austral.edu.ar>
2026-04-28 11:59:55 -0300

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -7,8 +7,11 @@ def flatten(matrix):
 
     Ejemplo: flatten([[1, 2], [3, 4], [5, 6]]) -> [1, 2, 3, 4, 5, 6]
     """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    answer = []
+    for lista in matrix:
+        for elemento in lista:
+            answer.append(elemento)
+    return answer
 
 def row_sums(matrix):
     """
@@ -17,7 +20,14 @@ def row_sums(matrix):
 
     Ejemplo: row_sums([[1, 2, 3], [4, 5, 6]]) -> [6, 15]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+
+    answer = []
+    for list in matrix:
+        suma = 0
+        for elemnto in list:
+            suma += elemnto
+        answer.append(suma)
+    return answer
 
 
 def col_sums(matrix):
@@ -28,4 +38,13 @@ def col_sums(matrix):
 
     Ejemplo: col_sums([[1, 2, 3], [4, 5, 6]]) -> [5, 7, 9]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(matrix) == 0:
+        return []
+    num_columns = len(matrix[0])
+    result = []
+    for col in range(num_columns):
+        total = 0
+        for fila in matrix:
+            total = total + fila[col]
+        result.append(total)
+    return result
